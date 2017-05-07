c636a55963beb2d0bf61a45ad854db425c003d49
Neil-Lal <neillal1992@gmail.com>
2017-05-07 12:29:03 -0700

    Update largerDemo.java

--- largerDemo.java
+++ largerDemo.java
@@ -10,7 +10,7 @@ public class largerDemo {
 	int x,y;
 	x = 3;
 	y = 6;
-	System.out.println(String.format("The larger of %d and %d is: %d",x,y,larger(x,y) ));
+	System.out.println(String.format("The larger of %d and %d is: %d".,x,y,larger(x,y) ));
     }
     
 }
